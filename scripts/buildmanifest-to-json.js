394128685397d5fbcca00e4b8c5123166c3f47d3
Glenn 'devalias' Grant <glenn@devalias.net>
2023-06-22 13:53:59 +1000

    refactor buildmanifest-to-json.js to take the build hash or URL to _buildManifest.js as a param

--- scripts/buildmanifest-to-json.js
+++ scripts/buildmanifest-to-json.js
@@ -1,32 +1,54 @@
 #!/usr/bin/env node
 
-const path = require('path');
-
 // TODO: Save these commands somewhere useful:
 //   ./buildmanifest-to-json.js | jq -r '[.. | strings | select(startswith("static"))] | unique | map("_next/" + .) | .[]' | pbcopy
 //   ./buildmanifest-to-json.js | jq -r '[.. | strings | select(startswith("static"))] | unique | map("https://chat.openai.com/_next/" + .) | .[]' | pbcopy
+
+const path = require('path');
 
 global.self = {};
 
 /**
  * Helper function to construct the path to the build manifest file
- * @param {string} chunkHash - The chunk hash to use in the path
+ * @param {string} buildHash - The build hash to use in the path
  * @return {string} - The full path to the build manifest file
  */
-function getBuildManifestPath(chunkHash) {
-  return path.join(__dirname, '..', 'orig', '_next', 'static', chunkHash, '_buildManifest.js');
+function getBuildManifestPath(buildHash) {
+  return path.join(__dirname, '..', 'orig', '_next', 'static', buildHash, '_buildManifest.js');
 }
 
-// const buildManifestPath = getBuildManifestPath('a3Jc7aP-UMfeR9s4-iLvW');
-// const buildManifestPath = getBuildManifestPath('Sz5I5M8XqNP7c6HNKaN0U');
-// const buildManifestPath = getBuildManifestPath('Wnd5NrsJwJ8L-68NTfmnl');
-// const buildManifestPath = getBuildManifestPath('2zx-gqs4DHnXCij8pMcM3');
-// const buildManifestPath = getBuildManifestPath('9rHv4XACEoIItIhKMAq4_');
-// const buildManifestPath = getBuildManifestPath('kwYJPc99Lx-XLUrLes-Se');
-const buildManifestPath = getBuildManifestPath('CTxpiEb45oE7hUCOWm4Jq');
+/**
+ * Helper function to extract build hash from URL
+ * @param {string} url - The URL to a _buildManifest.js file
+ * @return {string|null} - The extracted build hash or null if invalid
+ */
+function extractBuildHashFromURL(url) {
+  const match = url.match(/_next\/static\/([^/]+)\/_buildManifest\.js/);
+  return match ? match[1] : null;
+}
 
+// Get the input argument
+const inputArg = process.argv[2];
+
+// Validate the input argument
+if (!inputArg) {
+  console.error("Please provide a build hash or a URL to a _buildManifest.js file as an argument.");
+  process.exit(1);
+}
+
+const buildHash = inputArg.startsWith('http') ? extractBuildHashFromURL(inputArg) : inputArg;
+
+if (inputArg.startsWith('http') && !buildHash) {
+  console.error("Invalid URL. Make sure it points to a _buildManifest.js file.");
+  process.exit(1);
+}
+
+// Construct the full path to the build manifest file
+const buildManifestPath = getBuildManifestPath(buildHash);
+
+// Load the build manifest file
 require(buildManifestPath);
 
+// Output the JSON representation of the build manifest
 const buildManifestJSON = JSON.stringify(self.__BUILD_MANIFEST, null, 2);
-
-console.log(buildManifestJSON)
+console.log(buildManifestJSON);
